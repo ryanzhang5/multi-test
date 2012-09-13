f10ae508291ba85aa257e08553253b9796a96c36
linuxbox <linuxbox@gmail.com>
2012-09-13 03:55:27 -0400

    modfy

--- global-cache/src/main/java/com/wm/gc/l1/FileKey.java
+++ global-cache/src/main/java/com/wm/gc/l1/FileKey.java
@@ -1,5 +1,51 @@
 package com.wm.gc.l1;
 
-public class FileKey {
+import java.io.File;
+import com.wm.gc.util.GCUtil;
+import com.wm.gc.util.Conf;
 
+
+public class FileKey {
+  private static int maxDepth = Conf.getMaxDepth();                      // maximum number of directories deep
+  private static int chunkSize = Conf.getChunkSize();                     // characters in one "chunk" <--> directory name
+  //private static String dirDelim = File.separator;      // directory delimiter
+    
+    
+  public static String getFileName( String baseDir, CacheKey key, String version )  {
+    StringBuffer sb = new StringBuffer( baseDir );
+    String      hex = getHexString( key.getKey().getBytes() );
+
+    int nDirs = Math.min( ( hex.length() - 1 ) / chunkSize, maxDepth );
+    int start = 0;
+    for ( int i = 0; i < nDirs; i++ )
+    {
+      sb.append( hex.substring( start, start+chunkSize ) );
+      sb.append( File.separator );
+      start += chunkSize;
+    }
+    sb.append( hex.substring( start ) );
+    
+    if (version != null && !version.isEmpty()) {
+        sb.append(Conf.getVersion(version));
+      }
+    
+    return sb.toString();
+  }
+    
+  private static String getHexString( byte[] key )
+  {
+    if ( key == null ) return null;
+
+    long crc = GCUtil.calcChecksum( key );
+    String _crcHexFilename = Long.toHexString( crc );
+
+    return _crcHexFilename.toUpperCase();
+  }
+  
+  public static void main(String args[]){
+	  CacheKey key = CacheKey.getInstance("1075869");
+	  System.out.println(getFileName("/home/ryhzhang/", key,"1.0"));
+  }
+  
 }
+	
