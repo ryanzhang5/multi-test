02bd03e662772ce35eef569a5f41ecd06d69ce0d
ryanzhang5 <zyh_840819@163.com>
2012-10-08 12:34:04 +0800

    s

--- my-test/src/main/java/rmi/engine/ComputeEngine.java
+++ my-test/src/main/java/rmi/engine/ComputeEngine.java
@@ -19,10 +19,8 @@ public class ComputeEngine implements Compute {
 	}
 
 	
-	
 	 //java -cp /home/ryan/jone/public_html/classes/computer.jar:/home/ryan/jone/src  -Djava.rmi.server.codebase=file://localhost/home/ryan/jone/public_html/classes/  -Djava.security.policy=client.policy rmi.client.ComputePi
-	
-	
+	 //java -cp /home/ryan/ann/public_html/classes/computer.jar:/home/ryan/ann/src  -Djava.rmi.server.codebase=file://localhost/home/ryan/ann/public_html/classes/ -Djava.rmi.server.hostname=localhost -Djava.security.policy=server.policy rmi.engine.ComputeEngine
 	
 	
 	public static void main(String[] args) {
@@ -32,9 +30,9 @@ public class ComputeEngine implements Compute {
 		try {
 			String name = "Compute";
 			Compute engine = new ComputeEngine();
-			Compute stub = (Compute) UnicastRemoteObject
-					.exportObject(engine, 0);
-			Registry registry = LocateRegistry.getRegistry();
+			Compute stub = (Compute) UnicastRemoteObject.exportObject(engine, 0);
+			//Registry registry = LocateRegistry.getRegistry();
+			Registry registry = LocateRegistry.createRegistry(1099);
 			registry.rebind(name, stub);
 			System.out.println("ComputeEngine bound");
 		} catch (Exception e) {
