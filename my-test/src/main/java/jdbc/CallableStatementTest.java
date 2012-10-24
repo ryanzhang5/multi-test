7d8775cd5a636fd00ed12c6ab709919a83e8291b
ryanzhang5 <zyh_840819@163.com>
2012-10-24 08:50:43 +0800

    k

--- my-test/src/main/java/jdbc/CallableStatementTest.java
+++ my-test/src/main/java/jdbc/CallableStatementTest.java
@@ -8,43 +8,57 @@ import java.sql.SQLException;
 
 public class CallableStatementTest {
 
-	public static Connection getConnection()throws SQLException{
+	public static Connection getConnection() throws SQLException {
 		Connection connection = null;
 		String url = "jdbc:mysql://localhost:3306/hduo";
 		String user = "root";
 		String password = "123456";
-		connection = DriverManager.getConnection(url,user,password);
+		connection = DriverManager.getConnection(url, user, password);
 		return connection;
-		
+
 	}
-	
-	
-	public static void testCallable()throws SQLException{
+
+	public static void testCallable() throws SQLException {
 		String sql = "call GetUserByName3(?,?,?)";
 		Connection connection = getConnection();
 		CallableStatement cstmt = connection.prepareCall(sql);
-		
+
 		cstmt.setString(1, "paul");
 		cstmt.registerOutParameter(2, java.sql.Types.VARBINARY);
 		cstmt.registerOutParameter(3, java.sql.Types.INTEGER);
-		
 		ResultSet rs = cstmt.executeQuery();
-		
 		System.out.println(cstmt.getString(2) + "    " + cstmt.getInt(3));
 		while (rs.next()) {
-		System.out.println(rs.getInt(1) + "   " + rs.getString(2) + "   " + rs.getString(3));	
+			System.out.println(rs.getInt(1) + "   " + rs.getString(2) + "   "
+					+ rs.getString(3));
 		}
-		
+
 		rs.close();
 		cstmt.close();
 		connection.close();
-		
+
 	}
-	
-	
-	
-	public static void main(String[] args) throws SQLException{
-		testCallable();
+
+	public static void testCursor() throws SQLException {
+		String sql = "call GetAllUsers()";
+		Connection connection = getConnection();
+		CallableStatement cstmt = connection.prepareCall(sql);
+
+		cstmt.execute();
+
+		ResultSet rs = cstmt.getResultSet();
+
+		while (rs.next()) {
+			System.out.println(rs.getInt(1) + "   " + rs.getString(2) + "   "
+					+ rs.getString(3));
+		}
+		rs.close();
+		cstmt.close();
+		connection.close();
+	}
+
+	public static void main(String[] args) throws SQLException {
+		testCursor();
 	}
 
 }
