5ef9a5c75fddeb69914a0219a7deb6712337a02e
ryanzhang5 <zyh_840819@163.com>
2012-11-16 11:33:27 +0800

    re-import

--- hduo-erp/src/main/java/com/hduo/action/IncomeItemAction.java
+++ hduo-erp/src/main/java/com/hduo/action/IncomeItemAction.java
@@ -2,17 +2,14 @@ package com.hduo.action;
 
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
 
-import com.hduo.manager.ClientManager;
 import com.hduo.manager.IncomeItemManager;
 import com.hduo.manager.ProductManager;
-import com.hduo.pojo.Client;
 import com.hduo.pojo.IncomeItem;
 import com.hduo.pojo.Product;
 import com.hduo.util.Utils;
